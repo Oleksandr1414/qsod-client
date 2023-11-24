187fdecc3a60cf4f99d7a6c5b4bcda65a696854a
Oleksandr <olexander11441144@gmail.com>
2023-11-24 02:18:57 +0200

    add Home page design

--- src/components/generic/MiniRecipe.js
+++ src/components/generic/MiniRecipe.js
@@ -1,36 +1,36 @@
 import { useNavigate } from "react-router-dom";
 import { recipes } from "../../_helpers/data/recipes";
 
-import { ReactComponent as LikeIcon } from "./../../assets/LikeIcon.svg";
-import { ReactComponent as TimeIcon } from "./../../assets/TimeIcon.svg";
-
 import "./../../styles/generic/LineRecipe.css";
 import "./../../styles/generic/MiniRecipe.css";
 
-export default function MiniRecipe({ id, type }) {
+import { ReactComponent as LikeIcon } from "./../../assets/LikeIcon.svg";
+import { ReactComponent as TimeIcon } from "./../../assets/TimeIcon.svg";
+import { ReactComponent as Corner } from "./../../assets/Corner.svg";
+import { ReactComponent as SquareIcon } from "./../../assets/recipe/SquareIcon.svg";
+
+export default function MiniRecipe({ id }) {
   const recipe = recipes[id];
 
   const navigate = useNavigate();
 
   return (
-    <div
-      className={
-        type === "line" ? "recipe-line box-shadow" : "recipe-block box-shadow"
-      }
-      onClick={() => navigate(`/recipe/${id}`)}
-    >
+    <div className="recipe-block" onClick={() => navigate(`/recipe/${id}`)}>
+      <Corner />
+      <Corner />
+      <div className="link-to-recipe">
+        <button>
+          <SquareIcon />
+        </button>
+      </div>
       <div className="recipe-image">
         <img src={recipe.image_url} alt="" />
         <span className="recipe-likes">
-          55 <LikeIcon />
+          <LikeIcon /> 55
         </span>
         <span className="recipe-time">
-          20m <TimeIcon />
+          <TimeIcon /> 20m
         </span>
-      </div>
-      <div className="recipe-info">
-        <p className="recipe-name">{recipe.name}</p>
-        <p className="recipe-description">{recipe.description}</p>
       </div>
     </div>
   );
--- src/components/main-blocks/Home.js
+++ src/components/main-blocks/Home.js
@@ -4,24 +4,64 @@ import { userData } from "../../_helpers/data/user";
 
 import "./../../styles/main-blocks/Home.css";
 
+import { ReactComponent as Corner } from "./../../assets/Corner.svg";
+import { ReactComponent as ArrowRightUp } from "./../../assets/actions/ArrowRightUp.svg";
+import { ReactComponent as DoubleTopArrow } from "./../../assets/home-page/DoubleTopArrow.svg";
+
 export default function Home() {
   const navigate = useNavigate();
 
-  const lastRecipiesIds = userData.history.slice(0, 3);
+  const lastRecipiesIds = userData.history.slice(0, 1);
 
   return (
     <div className="main-container home-container">
-      <section className="link-to-generate  box-shadow">
-        <button onClick={() => navigate("/generation")}>
-          Go to generation
+      <div className="fly-block">
+        <Corner />
+        <Corner />
+        <Corner />
+        <Corner />
+        <span className="rounded-span">Accessible</span>
+        <p>Take control </p>
+        <p>Of your nutrition</p>
+        <p>By one click</p>
+      </div>
+      <div className="home-content-container">
+        <div>
+          <span className="rounded-span">Quickly</span>
+          <span className="rounded-span">Flexible</span>
+        </div>
+        <h2>
+          Fast
+          <br />
+          Diner
+          <br />
+          From AI
+        </h2>
+        <button
+          className="button-generation"
+          onClick={() => navigate("/generation")}
+        >
+          Go to generation <ArrowRightUp />
         </button>
-      </section>
-      <h3>Cooking history</h3>
-      <section className="history-container">
-        {lastRecipiesIds.map((id) => (
-          <MiniRecipe id={id} type={"block"} key={id} />
-        ))}
-      </section>
+        <div className="bottom-line">
+          {lastRecipiesIds.map((id) => (
+            <MiniRecipe id={id} key={id} />
+          ))}
+          <div className="history-link" onClick={() => navigate("/account")}>
+            History <DoubleTopArrow />
+          </div>
+          <div className="blog-link" onClick={() => navigate("/")}>
+            Blog <DoubleTopArrow />
+          </div>
+        </div>
+      </div>
+      <div className="home-picture-container">
+        <img
+          alt=""
+          src="https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq"
+        />
+        <div className="date-block">12.32.21</div>
+      </div>
     </div>
   );
 }
