21b203891f4ba0f7c4bc9343245bbed27c610ca8
Oleksandr <olexander11441144@gmail.com>
2023-11-25 16:01:39 +0200

    update FullRecipe style

--- src/_helpers/data/recipes.js
+++ src/_helpers/data/recipes.js
@@ -12,15 +12,24 @@ export const recipes = {
       { name: "garlic", quantity: "2 cloves" },
       { name: "spices", quantity: "to taste" },
     ],
-    formula:
-      "Cook the pasta, then prepare the sauce with tomatoes, garlic, and spices. Mix it with the pasta and serve hot.",
+    formula: `
+### Крок 1: Готування пасти
+- Сваріть пасту в окропленій воді згідно з інструкцією на упаковці.
+      
+### Крок 2: Приготування томатного соусу
+- Підготуйте соус із томатів, часнику та спецій.
+- Обсмажте часник на оливковому маслі, додайте нарізані томати та спеції. Тушкуйте, поки томати не розпустяться.
+      
+### Крок 3: Змішування та подача
+- Змішайте варену пасту з томатним соусом.
+- Подають гарячим.
+    `,
   },
   2: {
     id: 2,
     name: "Caesar Salad",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Classic Caesar salad with chicken.",
     ingredients: [
       { name: "chicken", quantity: "300 g" },
@@ -29,15 +38,23 @@ export const recipes = {
       { name: "parmesan", quantity: "50 g" },
       { name: "Caesar dressing", quantity: "3 tbsp" },
     ],
-    formula:
-      "Cook the chicken, mix it with lettuce, croutons, and parmesan. Add Caesar dressing and toss until well coated.",
+    formula: `
+### Крок 1: Приготування курки
+- Смажте курку до золотистої скорини.
+      
+### Крок 2: Змішування інгредієнтів
+- Перемішайте курку з салатом, гренками та пармезаном.
+- Додайте соус Цезар до смесі та добре перемішайте.
+      
+### Крок 3: Подача
+- Подають готовий салат.
+    `,
   },
   3: {
     id: 3,
     name: "Grilled Salmon",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Juicy grilled salmon with lemon and herbs.",
     ingredients: [
       { name: "salmon fillet", quantity: "2 pieces" },
@@ -46,15 +63,22 @@ export const recipes = {
       { name: "dill", quantity: "1 tbsp" },
       { name: "salt and pepper", quantity: "to taste" },
     ],
-    formula:
-      "Season salmon with salt, pepper, and dill. Grill until cooked. Squeeze lemon juice and drizzle olive oil before serving.",
+    formula: `
+### Крок 1: Підготовка лосося
+- Посоліть, поперчіть та обсмажте лосось на грилі до готовності.
+      
+### Крок 2: Приготування лимонного маринаду
+- Відцедіть сік з лимона, додайте нарізаний кріп та оливкове масло.
+      
+### Крок 3: Подача
+- Полийте лосось лимонним маринадом перед подачею.
+    `,
   },
   4: {
     id: 4,
     name: "Vegetarian Stir-Fry",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Colorful and nutritious vegetarian stir-fry.",
     ingredients: [
       { name: "tofu", quantity: "200 g" },
@@ -63,15 +87,22 @@ export const recipes = {
       { name: "bell peppers", quantity: "assorted colors, 1 cup" },
       { name: "soy sauce", quantity: "3 tbsp" },
     ],
-    formula:
-      "Stir-fry tofu and vegetables in soy sauce until tender. Serve hot.",
+    formula: `
+### Крок 1: Обсмаження таофу
+- Обсмажте таофу в соєвому соусі до золотистої скорини.
+      
+### Крок 2: Приготування овочів
+- Обсмажте овочі у соєвому соусі до тендітності.
+      
+### Крок 3: Подача
+- Змішайте таофу з овочами та подавайте гарячим.
+    `,
   },
   5: {
     id: 5,
     name: "Homemade Pizza",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Create your own pizza with your favorite toppings.",
     ingredients: [
       { name: "pizza dough", quantity: "1 ball" },
@@ -80,15 +111,22 @@ export const recipes = {
       { name: "pepperoni", quantity: "1/2 cup" },
       { name: "olives", quantity: "sliced, 1/4 cup" },
     ],
-    formula:
-      "Roll out the pizza dough, spread tomato sauce, add cheese and toppings. Bake until crust is golden and cheese is melted.",
+    formula: `
+### Крок 1: Розгортання тіста
+- Розгорніть тісто та розташуйте його на пекарському аркуші.
+      
+### Крок 2: Додавання начинки
+- Розподіліть томатний соус, моцарелу, пепероні та оливки по тісту.
+      
+### Крок 3: Підготовка та подача
+- Печіть піцу до золотисто-коричневого кольору. Подають гарячою.
+    `,
   },
   6: {
     id: 6,
     name: "Chicken Noodle Soup",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Comforting chicken noodle soup for any day.",
     ingredients: [
       { name: "chicken broth", quantity: "4 cups" },
@@ -97,15 +135,22 @@ export const recipes = {
       { name: "celery", quantity: "2 stalks" },
       { name: "egg noodles", quantity: "1 cup" },
     ],
-    formula:
-      "Boil chicken in broth, add vegetables and noodles. Simmer until chicken is cooked and flavors meld. Serve hot.",
+    formula: `
+### Крок 1: Варіння курячого бульйону
+- Приведіть курячий бульйон до кипіння.
+      
+### Крок 2: Додавання інгредієнтів
+- Додайте куряче філе, нарізані моркви, селеру та яйцяні локшини.
+      
+### Крок 3: Тушкування та подача
+- Тушкуйте до готовності курячого м'яса та подавайте гарячо.
+    `,
   },
   7: {
     id: 7,
     name: "Shrimp Scampi",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Garlicky shrimp scampi with a hint of lemon.",
     ingredients: [
       { name: "shrimp", quantity: "400 g" },
@@ -114,15 +159,22 @@ export const recipes = {
       { name: "butter", quantity: "3 tbsp" },
       { name: "lemon zest", quantity: "1 tsp" },
     ],
-    formula:
-      "Sauté shrimp and garlic in butter, toss with cooked spaghetti. Add lemon zest and serve.",
+    formula: `
+### Крок 1: Обсмаження креветок та часнику
+- Обсмажте креветки та часник на вершковому маслі до золотистого кольору.
+      
+### Крок 2: Додавання спагетті
+- Перемішайте обсмажені креветки та часник зі свареними спагетті.
+      
+### Крок 3: Посипка лимонної цедри та подача
+- Посипте страву лимонною цедрою перед подачею.
+    `,
   },
   8: {
     id: 8,
     name: "Caprese Salad",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description:
       "Light and refreshing Caprese salad with tomatoes and mozzarella.",
     ingredients: [
@@ -131,15 +183,23 @@ export const recipes = {
       { name: "fresh basil", quantity: "1 cup" },
       { name: "balsamic glaze", quantity: "2 tbsp" },
     ],
-    formula:
-      "Slice tomatoes and mozzarella, arrange with fresh basil. Drizzle balsamic glaze before serving.",
+    formula: `
+### Крок 1: Підготовка інгредієнтів
+- Наріжте томати та моцарелу тонкими кружечками.
+- Розмістіть їх разом з базиліком на подавальному блюді.
+      
+### Крок 2: Додавання бальзамічного глазуру
+- Полийте салат бальзамічним глазуром перед подачею.
+      
+### Крок 3: Подача
+- Подають негайно для максимального смаку.
+    `,
   },
   9: {
     id: 9,
     name: "Beef Stir-Fry",
     image_url:
       "https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq",
-
     description: "Quick and flavorful beef stir-fry with vegetables.",
     ingredients: [
       { name: "beef strips", quantity: "300 g" },
--- src/components/generic/FullRecipe.js
+++ src/components/generic/FullRecipe.js
@@ -1,21 +1,111 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 import { recipes } from "../../_helpers/data/recipes";
 import { useLocation } from "react-router-dom";
 
 import "./../../styles/generic/FullRecipe.css";
 
+import { ReactComponent as Play } from "./../../assets/actions/Play.svg";
+import { ReactComponent as Stop } from "./../../assets/actions/Stop.svg";
+import { ReactComponent as TimeIcon } from "./../../assets/TimeIcon.svg";
+
 export default function FullRecipe() {
+  const [isImerStart, setIsImerStart] = useState(false);
   const { pathname } = useLocation();
 
   const recipeInfo = useMemo(() => {
     if (!pathname) {
-      return {};
+      return null;
     }
 
     return recipes[pathname.split("/")[2]];
   }, [pathname]);
 
+  const formatRecipeSteps = useMemo(() => {
+    if (!recipeInfo) {
+      return null;
+    }
+
+    const lines = recipeInfo.formula.split("\n");
+    const steps = [];
+
+    for (const line of lines) {
+      if (line.trim().startsWith("###")) {
+        steps.push(
+          <p key={Math.random() * 100} className="recipe-steps-header">
+            {line.slice(3).trim()}
+          </p>
+        );
+      } else {
+        steps.push(<p key={Math.random() * 100}>{line}</p>);
+      }
+    }
+
+    return <div className="recipe-steps">{steps}</div>;
+  }, [recipeInfo]);
+
   return (
-    <div className="main-container recipe-container">{recipeInfo.name}</div>
+    <div className="main-container recipe-container">
+      {recipeInfo ? (
+        <>
+          <div className="recipe-components">
+            <div className="recipe-picture">
+              <img
+                alt=""
+                src="https://qph.cf2.quoracdn.net/main-qimg-c058b36cd1b52cb2d9fef42a7d9045eb-lq"
+              />
+            </div>
+            <div className="recipe-ingredients">
+              <p>List</p>
+              <div className="recipe-ingredients-list">
+                {recipeInfo.ingredients.map((i) => (
+                  <div key={i.name} className="ingredients-list-item">
+                    <span>ICON</span>
+                    <div className="vertical-line"></div>
+                    <p>{i.name}</p>
+                    <div className="vertical-line"></div>
+                    <span>{i.quantity}</span>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="recipe-main">
+            <div className="recipe-header">{recipeInfo.name}</div>
+            <div className="recipe-main-content">
+              <p>Step-by-step cooking plan:</p>
+              {formatRecipeSteps}
+            </div>
+          </div>
+          <div className="recipe-option">
+            <div className="recipe-controller">
+              <TimeIcon />
+              <div className="timer">00:00</div>
+              {isImerStart ? (
+                <button
+                  className="stop"
+                  onClick={() => {
+                    setIsImerStart(false);
+                  }}
+                >
+                  Stop <Stop />
+                </button>
+              ) : (
+                <button
+                  className="start"
+                  onClick={() => {
+                    setIsImerStart(true);
+                  }}
+                >
+                  Start <Play />
+                </button>
+              )}
+            </div>
+            <div className="recipe-more"></div>
+          </div>
+        </>
+      ) : (
+        "Recipe not found"
+      )}
+    </div>
   );
 }
